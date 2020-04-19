75c72498dc037603928745cac527bcb870ffffbe
Danny LEE <danny7616kimo@hotmail.com>
2020-04-19 14:52:25 +0800

    add new file

---
+++ 123.py
@@ -0,0 +1,16 @@
+x=5
+print(x)
+x=10
+y=2
+print(y)
+price=10
+pen=0.38
+rain=True
+name='Danny LEE'
+print(pen)
+print(pen)
+print(rain)
+print(name)
+print(price, pen, rain, name)
+name = input('請輸入名字:')
+print('HELLO!!', name)
